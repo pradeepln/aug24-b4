102f30f02f727f2b8cb2557a518c589eb3dbb678
Pradeep LN <pradeep.ln78@gmail.com>
2024-09-12 17:02:11 +0530

    after runnable impl

--- src/com/ibm/training/threads/ThreadDemo.java
+++ src/com/ibm/training/threads/ThreadDemo.java
@@ -3,12 +3,33 @@ package com.ibm.training.threads;
 public class ThreadDemo {
 
 	public static void main(String[] args) throws InterruptedException {
-		byExtendingThreadClass();
+		long startTime = System.currentTimeMillis();
+		
+		//byExtendingThreadClass();
+		byImplementingRunnable();
+		
+		long stopTime = System.currentTimeMillis();
+		System.out.println("Took "+(stopTime - startTime)+" ms.");
+	}
 
+	private static void byImplementingRunnable() throws InterruptedException {
+		
+		PrimeCounterTask task1 = new PrimeCounterTask(1, 140_000);
+		PrimeCounterTask task2 = new PrimeCounterTask(140_001, 200_000);
+		
+		Thread t1 = new Thread(task1);
+		Thread t2 = new Thread(task2);
+		
+		t1.start();
+		t2.start();
+		
+		t1.join();
+		t2.join();
+		System.out.println("There are "+(task1.getCount() + task2.getCount())+" primes b/w 1 and 200K");
 	}
 
 	private static void byExtendingThreadClass() throws InterruptedException {
-		long startTime = System.currentTimeMillis();
+		
 		
 		System.out.println("About to start prime example in "+Thread.currentThread().getName());
 		
@@ -26,8 +47,7 @@ public class ThreadDemo {
 		
 		System.out.println("There are "+(t1.getCount() + t2.getCount())+" primes between 1 and 200K");
 		
-		long stopTime = System.currentTimeMillis();
-		System.out.println("Took "+(stopTime - startTime)+" ms.");
+		
 	}
 
 }
